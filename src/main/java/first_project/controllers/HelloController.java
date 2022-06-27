746dbab9b3ad541e300c7becce6c0c21c43343fc
Anton Uzhva <anton.itec91@gmail.com>
2022-06-27 03:40:06 +0300

    Ver 9.0 Added bootstrap and finished it`s implementation for folder "view/person"

--- src/main/java/first_project/controllers/HelloController.java
+++ src/main/java/first_project/controllers/HelloController.java
@@ -16,64 +16,15 @@ import java.util.List;
 
 @Controller
 public class HelloController {
-    private final PersonService personService;
 
-    @Autowired
-    public HelloController(PersonService personService) {
-        this.personService = personService;
-    }
 
-    @GetMapping("/hello")
+    @GetMapping
     public String greeting() {
-        return "hello";
+        return "front_page";
     }
-
-//
-//    @GetMapping("/person/{id}")
-//    public String showPerson(@PathVariable(name = "id") String id, Model model) {
-//        int parsedId = Integer.parseInt(id);
-//        Person person = personService.getPersonById(parsedId);
-//       // System.out.println(person.getFullName());
-//        model.addAttribute("user", person.getFullName());
-//        return "person";
-//    }
-
-    @GetMapping("/allPeople")
-    public String showAllPeople(Model model) {
-        return "allPeople";
-    }
-
-    ///////Practice with RequestParam
-//    @GetMapping ("/preparedUrl")
-//        String prepareGetRequestWithParam () {
-//        return "prepareUrl";
-//        }
-
-
-    @GetMapping("/exampleGetRequest")
-    public String makeSomeActionWithGetRequestParam(@RequestParam(name= "name", required = false) String name,
-                                                    @RequestParam( name = "age") int age) {
-        System.out.println("My name is " + name + "\n I am " + age + " years old");
-        return "exampleGetRequest";
-    }
-
-
-    /////////
-
-
-    @ModelAttribute("person")
-    Person createDefault() {
-        Person person = new Person();
-        person.setFullName("guest");
-        return person;
-    }
-
-    @ModelAttribute("personList")
-    List<Person> createPersonList() {
-        List<Person> personList = new ArrayList<>();
-        personList = personService.getAllPeople();
-        return personList;
-    }
-
-
 }
+
+
+
+
+
